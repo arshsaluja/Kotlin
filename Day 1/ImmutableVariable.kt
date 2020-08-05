5ebe0a525768400e42c50fceedbf47929d09de6f
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 00:35:20 +0530

    using val in variable declaration -value of variable is static ,constant

---
+++ Day 1/ImmutableVariable.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>)
+{
+  val name="Arsh"
+  println(name)
+  //we cannot change the value
+  //if we do so
+  /*
+  name="Ashmeet"
+  println(name)
+  */
+  //it will display an error -val cannot be reassigned
+}
