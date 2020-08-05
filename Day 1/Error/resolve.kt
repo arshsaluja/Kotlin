210e5fcf80fb88ba44034785a29838e275c14b2d
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 00:45:29 +0530

    Error Resolved

---
+++ Day 1/Error/resolve.kt
@@ -0,0 +1,14 @@
+//this problem can be resolved by this:
+//this is another way of declaring a variable
+fun main(args: Array<String>)
+{
+  val name:String
+  name="Arsh"
+  println(name)
+  //or if we want an integer variable
+  /*
+  val age:Int
+  age=20
+  println(age)
+  */
+}
