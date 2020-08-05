45e54fb261d4180aaef8feb6d6097bee30a40f7a
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 02:34:01 +0530

    Here Variable value- does not need declaration

---
+++ Day 1/Programs/For Value/imp.kt
@@ -0,0 +1,9 @@
+fun main(args: Array <String>)
+{
+  for(value in 1..10)
+  {
+   println("$value")
+  }
+}
+//here value is a psedo variable &will only be declared inside for loop
+//Variable value does not need declaration
