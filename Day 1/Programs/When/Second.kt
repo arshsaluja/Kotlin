d8ffa78ce163dfe114eeaf2121a623d1cb65139c
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 02:16:57 +0530

    Like Switch Case in java - little simple

---
+++ Day 1/Programs/When/Second.kt
@@ -0,0 +1,12 @@
+fun main(args: Array <String>)
+{
+  var day=3
+  when (day)
+  {
+  1, 2, 3, 4, 5 -> println("Weekday")
+
+  6, 7 -> println("Weekend")
+  
+  else -> println("Invalid Day")//Deafault
+  }
+}
