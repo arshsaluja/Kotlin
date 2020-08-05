7112ca405632d64e718f4ca2b7da07f148cd69a8
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 01:41:50 +0530

    Integer - String - Float

---
+++ Day 1/Type Casting/Conversion.kt
@@ -0,0 +1,18 @@
+fun main(args: Array <String>)
+{
+  //toString()
+  var num=100
+  num.toString()
+  println(num)
+  
+  //toInt()
+  var number="100"
+  number.toInt()
+  println(number)
+  
+  //toFloat()
+  var no=100
+  no.toFloat()
+  println(no)
+
+}
