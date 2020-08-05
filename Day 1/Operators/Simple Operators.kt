b57035aaab9d56e69784bd1036d9bb57264fc19e
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 01:47:50 +0530

    Simple BODMAS rules

---
+++ Day 1/Operators/Simple Operators.kt
@@ -0,0 +1,17 @@
+fun main(args: Array <String>)
+{
+  var a=10
+  var b=20
+  var c=30
+  var d=(a+b)*(c+a)/a //BODMAS Rule
+  println(d)
+  d++
+  println(d)
+  d+=5
+  println(d)
+  //OUTPUT:
+  //120
+  //121
+  //126
+
+}
