18b567ece86d93684815d97abfeea0d36b35250e
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 00:55:02 +0530

    //Types of arrays

---
+++ Day 1/Array/TypesOfArray.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>)
+{
+  //Integer array
+  var numbers=arrayOf(1,2,3,4)
+  //String array
+  var names=arrayOf("John","Sam","Mohan","Raja")
+  //Mixed array
+  var mixed=arrayOf(1,"John",2,"Sam",3,"Mohan",4,"Raja")
+}
