ecc1a28177f50290a3a4030777419a2c301327ba
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 01:52:10 +0530

    Use Function on + - * /

---
+++ Day 1/Operators/Use Functions.kt
@@ -0,0 +1,20 @@
+fun main(args: Array <String>)
+{
+  var a=10
+  var b=20
+  var c=30
+  var d=(a+b)*(c+a)/a //BODMAS Rule
+  //Addition
+  println(d.plus(b))
+  //Substraction
+  println(d.minus(b))
+  //Multiplication
+  println(d.times(b))
+  //Division
+  println(d.div(b))
+  //OUTPUT
+  //140
+  //100
+  //2400
+  //6
+}
