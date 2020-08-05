20dae1690a05c74d4240d36488154ef7c4047d22
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-06 00:38:45 +0530

    This code will show an error

---
+++ Day 1/Error/error.kt
@@ -0,0 +1,8 @@
+//This code will show an error
+//as it will not go to the next line and ony display error in line - val name
+fun main(args: Array<String>)
+{
+  val name
+  name="Arsh"
+  println(name)
+}
