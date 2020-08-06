21745a67224c3854c19e7a215307ecfe63379257
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-07 03:08:22 +0530

    importance of null and not null

---
+++ Day 2/Nullable & Nonnullable/Code2.kt
@@ -0,0 +1,11 @@
+
+fun main(args: Array<String>)
+{
+    var name:String ="Arsh"
+    if(name!=null)
+        println("Hello $name")
+    else
+        println("Hello guest")
+}
+//OUTPUT:
+//Hello guest
