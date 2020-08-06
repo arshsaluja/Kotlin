9a4e28fbe36ba727af6ab1de8c679f29b4260a62
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-07 01:33:01 +0530

    It is an integer array only

---
+++ Day 2/Array/intArrayOf.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>)
+{
+    //it is an integer array we cannot enter string or other values
+    var n= intArrayOf(1,2,3,4,5)
+    for(num in n)
+    {
+        println("$num")
+    }
+}
