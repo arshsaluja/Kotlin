a9fbe22d674d5f5368a969cad64af68684c87d45
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-08 01:28:21 +0530

    readLine() -It always takes input as String

--- Day 3/User Input/Code1.kt
+++ Day 3/User Input/Code1.kt
@@ -1,7 +1,11 @@
 fun main(args:Array<String>)
 {
     println("Enter your name:")
-
+    
+    //!!- represents not null expressions
+    
+    //readLine() -It always takes input as String
+    
     var name= readLine()!!
 
     println("Your name is $name")
