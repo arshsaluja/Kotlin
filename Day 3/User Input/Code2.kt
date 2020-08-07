2de165ebb950d62fc504c4b77186c44f3374535c
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-08 01:36:28 +0530

    We take  Integer.valueOf(readline()!!)

---
+++ Day 3/User Input/Code2.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>)
+{
+    println("Enter your number:")
+
+    //!!- represents not null expressions
+    //readLine() -It always takes input as String
+    //so we take  Integer.valueOf(readline()!!)
+    
+    
+    var number = Integer.valueOf(readLine()!!)
+
+    println("Your number is $number")
+}
