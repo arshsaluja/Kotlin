91f7d6052b1755bda754df1fc238e8e2a7acf953
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-08 17:41:20 +0530

    Another Way by using +

---
+++ Day 3/Infix Notations/Code3.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>)
+{
+    var namemap=mapOf(1 to "Ram",2 to "Sid",3 to "John")
+    //1 is the key and "Arsh" is the value
+    //just as in hashmap in java
+    println("Map values are at loc 2:" +namemap[2])
+    //prints infix notation at given index
+
+}
+//Another Way by using +
