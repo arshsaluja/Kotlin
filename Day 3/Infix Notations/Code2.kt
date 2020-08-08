b03da39196d8eb525db39700d4ae224c0824d1a2
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-08 17:39:13 +0530

    prints full infix notation

---
+++ Day 3/Infix Notations/Code2.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>)
+{
+    var namemap=mapOf(1 to "Ram",2 to "Sid",3 to "John")
+    //1 is the key and "Arsh" is the value
+    //just as in hashmap in java
+    println("Map values are:" +namemap)
+    //prints full infix notation
+
+}
