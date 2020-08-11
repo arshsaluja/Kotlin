04bbd0cf8700b66981a1775646179c4b06cb7462
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-11 13:41:12 +0530

    an object can hold multiple categories

---
+++ Day 6/Descructive Declaration/Code1.kt
@@ -0,0 +1,13 @@
+
+//Descructive Declaration
+//split and multi variable use!!
+//an object can hold multiple categories
+
+data class Person(var name:String,var age:Int,var roll:Int)
+fun main(args:Array<String>)
+{
+    var (a,b,c)=Person("Arsh",20,17)
+    println("The name of the data class is ${a}")
+    println("The age of the data class is ${b}")
+    //here we can only display the values we need
+}
